99e6fab60223ba9ef8a2b61604604819f63b526a
Les Wright <lesliewright1977@gmail.com>
2022-10-18 21:53:25 +0100

    Update PySpectrometer2-Picam2-v1.0.py

--- src/PySpectrometer2-Picam2-v1.0.py
+++ src/PySpectrometer2-Picam2-v1.0.py
@@ -10,7 +10,7 @@ This project is a follow on from: https://github.com/leswright1977/PySpectromete
 This is a more advanced, but more flexible version of the original program. Tk Has been dropped as the GUI to allow fullscreen mode on Raspberry Pi systems and the iterface is designed to fit 800*480 screens, which seem to be a common resolutin for RPi LCD's, paving the way for the creation of a stand alone benchtop instrument.
 
 Whats new:
-Higher reolution (800px wide graph)
+Higher resolution (800px wide graph)
 3 row pixel averaging of sensor data
 Fullscreen option for the Spectrometer graph
 3rd order polymonial fit of calibration data for accurate measurement.
@@ -22,10 +22,6 @@ Key Bindings for all operations
 All old features have been kept, including peak hold, peak detect, Savitsky Golay filter, and the ability to save graphs as png and data as CSV.
 
 For instructions please consult the readme!
-
-Future work:
-It is planned to add in GPIO support, to allow the use of buttons and knobs to control the Spectrometer.
-
 '''
 
 
