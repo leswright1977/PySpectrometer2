b6872769fc31e63ae8c7896e4920626b258759ba
Daenks <Daenks@users.noreply.github.com>
2024-05-23 09:37:48 -0500

    Update specFunctions.py
    Update for numpy runtime errors. np.int is deprecated and crashed the software, this simple fix makes it work again.

--- src/specFunctions.py
+++ src/specFunctions.py
@@ -124,8 +124,8 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
 	import numpy as np
 	from math import factorial
 	try:
-		window_size = np.abs(np.int(window_size))
-		order = np.abs(np.int(order))
+		window_size = np.abs(int(window_size))
+		order = np.abs(int(order))
 	except ValueError:
 		raise ValueError("window_size and order have to be of type int")
 	if window_size % 2 != 1 or window_size < 1:
